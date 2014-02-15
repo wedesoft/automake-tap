91aab321587c5245583ea89c8c6f8b6c6c52e606
Yawar Amin <yawar.amin@gmail.com>
2014-02-14 22:01:44 -0500

    tests: use ggspec

---
+++ foo.scm
@@ -0,0 +1,37 @@
+#!/usr/bin/guile --no-auto-compile
+!#
+(use-modules (ggspec lib))
+
+(suite "foo"
+  (tests
+    (test "Swallows fly"
+      e (assert-equal 1 1))
+    (test "Caterpillars fly"
+      e
+      (assert-equal
+        "caterpillars fly"
+        "caterpillars dont fly"))
+    (test "Pigs fly"
+      e (assert-true #f)
+      (options
+        (option 'skip #t)))
+    (test "Flies fly too :-}"
+      e
+      (assert-equal
+        "Fly"
+        (string-append (substring "Flies" 0 2) "y"))))
+  (options
+    (option 'output-cb output-tap)
+    (option 'tally #t)))
+
+#!
+echo '# Suite: foo'
+echo 'ok - Swallows fly'
+echo 'not ok - Caterpillars fly # TODO metamorphosis in progress'
+echo '# Expected: caterpillars fly'
+echo '#      Got: caterpillars dont fly'
+echo 'ok - Pigs fly # SKIP not enough acid'
+echo 'ok - Flies fly too :-)'
+echo 1..4 # Number of tests to be executed.
+!#
+
